d6d756b27b29eca9997316234711e56de16525c5
Prajwal <prajwal.zurunge12@gmail.com>
2021-10-17 17:55:52 +0530

    Creating board program

--- TIcTacToe-Day7/Program.cs
+++ TIcTacToe-Day7/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
 
-namespace TIcTacToe_Day7
+namespace TicTacToeGame
 {
     class Program
     {
+        //making array and   where no use of zero 
+
+        static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+        static int player = 1; //By default player 1 is set  
+
+        static int choice; //This holds the choice at which position user want to mark   
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to TicTacToe Challenge");
+            Board();
+        }
+
+        // create board method
+        private static void Board()
+
+        {
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[1], arr[2], arr[3]);
+
+            Console.WriteLine("_____|_____|_____ ");
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[4], arr[5], arr[6]);
+
+            Console.WriteLine("_____|_____|_____ ");
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[7], arr[8], arr[9]);
+
+            Console.WriteLine("     |     |      ");
         }
     }
 }
